401995eab0a0e8850ee4bbdf3b3cf4c8f916578b
Tetiana Zdorenko <tanyazdorenkoo@gmail.com>
2025-09-05 10:22:35 +0300

    corrected mistake

--- cypress.config.qautoLogin.js
+++ cypress.config.qautoLogin.js
@@ -3,9 +3,5 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
     e2e: {
         baseUrl: 'https://guest:welcome2qauto@qauto.forstudy.space',
-        env: {
-            useremail: 'tanyazdorenkoo+user@gmail.com',
-            password: 'ValidPass1',
-        },
     },
 });
